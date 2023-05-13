90886370518eb90e93d998511eeb7e69d2cd3522
Khaira Isyara <khairaisyara25@gmail.com>
2023-05-13 18:06:05 +0700

    update home

--- index.php
+++ index.php
@@ -53,143 +53,10 @@
     </div>
     </nav>
 
-    <!-- carosel -->
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-inner" style='height: 410px;'>
-        <div class="carousel-item active">
-        <img src="img/promo1.jpg" class="d-block w-100" style='height: 410px;' alt="">
-        </div>
-        <div class="carousel-item">
-        <img src="img/promo2.jpg" class="d-block w-100" style='height: 410px;' alt="">
-        </div>
-        <div class="carousel-item">
-        <img src="img/promo3.jpg" class="d-block w-100" style='height: 410px;' alt="">
-        </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-    </div>
-
-    <div class="container">
-        <h2>Film Terlaris</h2>
-        <div class="row">
-            <div class="col-2 mb-3">
-                <img src="img/avatar.jpg" class="card-img-top" alt="">
-            </div>
-            <div class="col-2 mb-3">
-                <img src="img/avatar.jpg" class="card-img-top" alt="">
-            </div>
-            <div class="col-2 mb-3">
-                <img src="img/avatar.jpg" class="card-img-top" alt="">
-            </div>
-            <div class="col-2 mb-3">
-                <img src="img/avatar.jpg" class="card-img-top" alt="">
-            </div>
-            <div class="col-2 mb-3">
-                <img src="img/avatar.jpg" class="card-img-top" alt="">
-            </div>
-            <div class="col-2 mb-3">
-                <img src="img/avatar.jpg" class="card-img-top" alt="">
-            </div>
-            <div class="col-2 mb-3">
-                <img src="img/avatar.jpg" class="card-img-top" alt="">
-            </div>
-        </div>
-    </div>
-    <!-- card -->
-    <section class="p-5">
-    <h2 style="padding-left: 10px;">Film Terlaris</h2>
-        <div class="container">
-            <div class="row text-center g-4 ">
-                <div class="col-md">
-                    <img src="img/avatar.jpg" class="card-img-top" alt="">
-                    </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/avengers.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/titanic.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/avengers1.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/spiderman.jpg" class="card-img-top" alt=""> 
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/jurasic.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/lion.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="p-6">
-    <h2 style="padding-left: 55px;">Apa yang Baru?</h2>
-        <div class="container">
-            <div class="row text-center g-4">
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/antman.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/gotg1.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/creed.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/marvels.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/aquaman.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/mermaid.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-                <div class="col-md">
-                    <div class="card-body">
-                    <img src="img/jhon.jpg" class="card-img-top" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-<br></br>
+    
 
     <footer class="text-white pt-5" style="background-color: rgb(29, 34, 40)">
-        <!--<div class="footer-top mt-20 container">
+        <div class="footer-top mt-20 container">
             <div class="row gy-4">
                 <div class="col-lg-3">
                     <img class="logo w-50" src="img/bisokop online.png" alt="">
@@ -225,7 +92,7 @@
                         </div>
                     </div>
                 </div>
-            </div>-->
+            </div>
     </footer>
 </body>
 
---
+++ pages/home.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- carosel -->
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner" style='height: 410px;'>
+        <div class="carousel-item active">
+        <img src="img/promo1.jpg" class="d-block w-100" style='height: 410px;' alt="">
+        </div>
+        <div class="carousel-item">
+        <img src="img/promo2.jpg" class="d-block w-100" style='height: 410px;' alt="">
+        </div>
+        <div class="carousel-item">
+        <img src="img/promo3.jpg" class="d-block w-100" style='height: 410px;' alt="">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+    </div>
+
+    <div class="container">
+        <h2>Film Terlaris</h2>
+        <div class="row">
+            <div class="col-2 mb-3">
+                <img src="img/avatar.jpg" class="card-img-top" alt="">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="img/avatar.jpg" class="card-img-top" alt="">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="img/avatar.jpg" class="card-img-top" alt="">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="img/avatar.jpg" class="card-img-top" alt="">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="img/avatar.jpg" class="card-img-top" alt="">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="img/avatar.jpg" class="card-img-top" alt="">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="img/avatar.jpg" class="card-img-top" alt="">
+            </div>
+        </div>
+    </div>
+    <!-- card -->
+    <section class="p-5">
+    <h2 style="padding-left: 10px;">Film Terlaris</h2>
+        <div class="container">
+            <div class="row text-center g-4 ">
+                <div class="col-md">
+                    <img src="img/avatar.jpg" class="card-img-top" alt="">
+                    </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/avengers.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/titanic.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/avengers1.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/spiderman.jpg" class="card-img-top" alt=""> 
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/jurasic.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/lion.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="p-6">
+    <h2 style="padding-left: 55px;">Apa yang Baru?</h2>
+        <div class="container">
+            <div class="row text-center g-4">
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/antman.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/gotg1.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/creed.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/marvels.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/aquaman.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/mermaid.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+                <div class="col-md">
+                    <div class="card-body">
+                    <img src="img/jhon.jpg" class="card-img-top" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+<br></br>
+</body>
+</html>
