7ab2708cd5ec8af2b7d99a073f309bfe96b30e23
Khaira Isyara <khairaisyara25@gmail.com>
2023-05-19 14:13:01 +0700

    reset pass

---
+++ pages/lupapassword.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Reset Password</title>
+</head>
+<body>
+<section class="vh-100" style="background-image: url('../img/bg.jpg'); background-size: cover;">
+  <div class="container py-2 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-6 w-50">
+        <div class="card justify-content-center" style="border-radius: 1rem; background-color: #1F1F1F; width: 546px; height: 500px;">
+          <div class="row g-0">
+            <div class="col-md-12 col-lg-12 d-flex align-items-center">
+              <div class="card-body p-2 p-lg-5 text-black">
+
+                <form class="justify-content-center">
+
+                <h1 class="text-center mb-5">Reset Password</h1>
+
+                  <div class="form-outline mb-4">
+                    <label class="" for="email">Email</label>
+                    <input type="email" id="form2Example17" class="form-control form-control-lg" placeholder="Email Address" />
+                  </div>
+
+                  <div class="pt-1 mb-4 text-center">
+                    <button class="btn btn-dark btn-lg rounded-pill" type="button" style="background-color: #113250; width: 170px; height: 50px;">Lanjut</button>
+                  </div>
+
+                  <div class="text-center">
+                    <a href="register.php"
+                      style="color: #70AFE9;">< Kembali</a>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
---
+++ pages/lupapassword2.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Reset Password</title>
+</head>
+<body>
+<section class="vh-100" style="background-image: url('../img/bg.jpg'); background-size: cover;">
+  <div class="container py-2 h-100">
+    <div class="row d-flex justify-content-center align-items-center h-100">
+      <div class="col col-xl-6 w-50">
+        <div class="card justify-content-center" style="border-radius: 1rem; background-color: #1F1F1F; width: 546px; height: 500px;">
+          <div class="row g-0">
+            <div class="col-md-12 col-lg-12 d-flex align-items-center">
+              <div class="card-body p-2 p-lg-5 text-black">
+
+                <form class="justify-content-center">
+
+                <h1 class="text-center mb-5">Reset Password</h1>
+
+                  <div class="form-outline mb-3">
+                    <label class="form-label" for="password">Password</label>
+                    <input type="password" id="form2Example27" class="form-control form-control-lg" placeholder="Password" />
+                  </div>
+
+                  <div class="form-outline mb-5">
+                    <label class="form-label" for="password">Konfirmasi Password</label>
+                    <input type="password" id="form2Example27" class="form-control form-control-lg" placeholder="Konfirmasi Password" />
+                  </div>
+
+                  <div class="pt-1 text-center">
+                    <button class="btn btn-dark btn-lg rounded-pill" type="button" style="background-color: #113250; width: 170px; height: 50px;">Lanjut</button>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</body>
+</html>
