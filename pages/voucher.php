9b620d95b1e2f7b1b76b39a09b8c150d85bf3e89
ChiekalMulia <75968931+ChiekalMulia@users.noreply.github.com>
2023-06-05 14:04:42 +0700

    vocer
    blm

--- pages/voucher.php
+++ pages/voucher.php
@@ -13,15 +13,7 @@
 
 </head>
 
-<body class="movie bg-dark" style="background-color: #1F1F1F;"><br></br>
-<div class="voucher" style="width: 18rem;">
-  <img src="./img/v1.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+<body class="movie bg-dark" style="background-color: #1F1F1F;">
+  
 </body>
-
 </html>
