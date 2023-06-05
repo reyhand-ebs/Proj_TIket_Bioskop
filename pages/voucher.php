1f1d9f0ac2492c769d66e11d888da532c881c44f
ChiekalMulia <75968931+ChiekalMulia@users.noreply.github.com>
2023-06-05 14:35:45 +0700

    vocer
    blm final

--- pages/voucher.php
+++ pages/voucher.php
@@ -14,6 +14,29 @@
 </head>
 
 <body class="movie bg-dark" style="background-color: #1F1F1F;">
-  
+<div class="container">
+        <h2 class="text-white">What's New</h2>
+        <div class="row">
+            <div class="col-2 mb-3">
+                <img src="https://media.21cineplex.com/webcontent/gallery/pictures/168498920013335_240x360.jpg" class="card-img-top" style="border-radius: 15px;">
+                <p class="card-text text-white">Some quick.</p>
+            </div>
+            <div class="col-2 mb-3">
+                <img src="https://media.21cineplex.com/webcontent/gallery/pictures/1685530484885_240x360.jpg" class="card-img-top" style="border-radius: 15px;">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="https://media.21cineplex.com/webcontent/gallery/pictures/168448069635119_240x360.jpg" class="card-img-top" style="border-radius: 15px;">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="https://media.21cineplex.com/webcontent/gallery/pictures/167352378417407_240x360.jpg" class="card-img-top" style="border-radius: 15px;">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="https://media.21cineplex.com/webcontent/gallery/pictures/167352378417407_240x360.jpg" class="card-img-top" style="border-radius: 15px;">
+            </div>
+            <div class="col-2 mb-3">
+                <img src="https://media.21cineplex.com/webcontent/gallery/pictures/167404472740509_208x311.jpg" class="card-img-top" style="border-radius: 15px;">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
