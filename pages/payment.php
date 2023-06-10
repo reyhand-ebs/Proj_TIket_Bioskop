54482a5593c7ccd17a450f0f1f17c60eb35c7535
ChiekalMulia <75968931+ChiekalMulia@users.noreply.github.com>
2023-06-10 10:09:03 +0700

    final
    closing

--- pages/payment.php
+++ pages/payment.php
@@ -130,13 +130,13 @@
                     <div class="modal-dialog" role="document">
                         <div class="modal-content">
                         <div class="modal-header text-dark">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 class="modal-title" id="exampleModalLabel">Payment</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body text-dark">
-                            ...
+                            your transaction is success
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
